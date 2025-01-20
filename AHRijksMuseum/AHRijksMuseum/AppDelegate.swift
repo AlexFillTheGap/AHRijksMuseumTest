37fec8d20fc4250195661e55d71920c3587a3768
Alex Fernandez <alejandrofernandezruiz8@gmail.com>
2025-01-20 18:10:24 +0100

    First app setup, Basic colors, Remove main storyboard and install SwiftLint

---
+++ AHRijksMuseum/AHRijksMuseum/AppDelegate.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    let color = UIColor.accent
+    let navigationBarAppearace = UINavigationBar.appearance()
+    navigationBarAppearace.titleTextAttributes = [.foregroundColor: color]
+    navigationBarAppearace.tintColor = UIColor.accent
+    navigationBarAppearace.backgroundColor = UIColor.background
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+  func application(
+    _ application: UIApplication,
+    didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+  ) { }
+}

36f742c2977f271095a62c7e51b4ce2a8bd8940e
Alex Fernandez <alejandrofernandezruiz8@gmail.com>
2025-01-21 18:04:08 +0100

    setup more info view

--- AHRijksMuseum/AHRijksMuseum/Scenes/MoreInfo/MoreInfoViewController.swift
+++ AHRijksMuseum/AHRijksMuseum/Scenes/MoreInfo/MoreInfoViewController.swift
@@ -8,6 +8,10 @@ class MoreInfoViewController: UIViewController {
     private var requests: MoreInfoRequests?
     var router: (MoreInfoRouting & MoreInfoDataPassing)?
 
+    private let scrollView = Views.scrollView()
+    private let imageView = Views.imageView()
+    private let titleLabel = Views.titleLabel()
+    private let descriptionLabel = Views.descriptionLabel()
     private let loaderView = UIActivityIndicatorView.loaderView()
 
     // MARK: Object lifecycle
@@ -36,14 +40,96 @@ class MoreInfoViewController: UIViewController {
         super.viewDidLoad()
         setupView()
         loaderView.isHidden = false
+        Task {
+            await requests?.doLoadInitialData(request: MoreInfoInitialData.Request())
+        }
     }
 
     private func setupView() {
         view.backgroundColor = UIColor.background
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(titleLabel)
+        scrollView.addSubview(descriptionLabel)
+        scrollView.addSubview(loaderView)
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+
+            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 300),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+
+            loaderView.centerXAnchor.constraint(equalTo: descriptionLabel.centerXAnchor),
+            loaderView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            descriptionLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16)
+        ])
     }
 }
 
 extension MoreInfoViewController: MoreInfoView {
-    func displayLoadedData(view: MoreInfoInitialData.View) {}
+    func displayLoadedData(view: MoreInfoInitialData.View) {
+        DispatchQueue.main.async {
+            self.title = view.artViewModel.screenTitle
+            self.titleLabel.text = view.artViewModel.title
+            Task {
+                let (image, _) = await ImageCache.shared.load(urlString: view.artViewModel.imageUrlString)
+                UIView.transition(
+                    with: self.imageView,
+                    duration: 0.3,
+                    options: .transitionCrossDissolve,
+                    animations: {
+                        self.imageView.image = image
+                    },
+                    completion: nil
+                )
+            }
+        }
+    }
+}
 
+// MARK: - UI
+
+private enum Views {
+    @MainActor static func scrollView() -> UIScrollView {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }
+
+    @MainActor static func imageView() -> UIImageView {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }
+
+    @MainActor static func titleLabel() -> UILabel {
+        let title = UILabel()
+        title.font = UIFont.boldSystemFont(ofSize: 20)
+        title.numberOfLines = 0
+        title.lineBreakMode = .byClipping
+        title.translatesAutoresizingMaskIntoConstraints = false
+        return title
+    }
+
+    @MainActor static func descriptionLabel() -> UILabel {
+        let description = UILabel()
+        description.numberOfLines = 0
+        description.lineBreakMode = .byClipping
+        description.translatesAutoresizingMaskIntoConstraints = false
+        return description
+    }
 }
